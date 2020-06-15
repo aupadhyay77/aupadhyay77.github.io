8f64ee1e679e75beea2108f4445503c8239c6282
Arun Upadhyay <arunstar121@gmail.com>
2020-06-15 08:28:52 +0530

    Updates

---
+++ precache-manifest.b8a2274cacbf42979c990cc980a9264f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d46f29c603c57c3017e3515a193819",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "e2b4f4b7354599942b5b",
+    "url": "/reactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5153b06881fdf866dc97",
+    "url": "/reactApp/static/js/2.d0f229c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactApp/static/js/2.d0f229c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b4f4b7354599942b5b",
+    "url": "/reactApp/static/js/main.5f5fe05b.chunk.js"
+  },
+  {
+    "revision": "cb1d4beeb22a8e42edf5",
+    "url": "/reactApp/static/js/runtime-main.449f16cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.51965a3650676b0300dbc3c48848f7e1.js"
+  "/reactApp/precache-manifest.b8a2274cacbf42979c990cc980a9264f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
